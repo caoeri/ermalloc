9ff483e83f651779edef3d16ec63663c3235aa2c
Dillon Hammond <dillonhammond@gmail.com>
2020-02-08 13:37:22 -0600

    defining some conversions

--- src/ffi.rs
+++ src/ffi.rs
@@ -2,6 +2,8 @@ use libc::*;
 
 use std::ptr;
 
+use crate::policies::Policy;
+
 #[repr(C)]
 #[derive(Copy, Clone)]
 pub enum ErPolicyRaw {
@@ -14,6 +16,28 @@ pub struct ErPolicyListRaw {
     policy: ErPolicyRaw,
     policy_data: *const c_void,
     er_list_policy_raw: *const ErPolicyListRaw,
+}
+
+pub struct ErPolicyListNonNull {
+    policy: ErPolicyRaw,
+    policy_data: ptr::NonNull<c_void>,
+    er_list_policy_raw: Option<ptr::NonNull<ErPolicyListNonNull>>,
+}
+
+impl From<ErPolicyListNonNull> for Policy {
+    fn from(raw: ErPolicyListNonNull) -> Self {
+        match raw.policy {
+        ErPolicyRaw::Nil => Policy::Nil,
+        ErPolicyRaw::Redundancy => {
+            let ptr = raw.policy_data.clone().cast::<u32>();
+            let num;
+            unsafe {
+                num = *(ptr.as_ptr());
+            }
+            Policy::Redundancy(num)
+        },
+        }
+    }
 }
 
 #[no_mangle]
