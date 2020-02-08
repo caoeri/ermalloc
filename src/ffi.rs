4bcf94e1af9be2b206547ea75c7886aa2ef586ba
Dillon Hammond <dillonhammond@gmail.com>
2020-02-08 13:09:01 -0600

    basic ffi

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,4 +1,16 @@
+use libc::*;
 
 #[repr(C)]
-enum Er
+#[derive(Copy, Clone)]
+enum ErPolicyRaw {
+    Nil,
+    Redundancy,
+}
+
+#[repr(C)]
+struct ErPolicyListRaw {
+    policy: ErPolicyRaw,
+    policy_data: *const c_void,
+    er_list_policy_raw: *const ErPolicyListRaw,
+}
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-mod policies;
+//mod policies;
+mod ffi;
 
 #[cfg(test)]
 mod tests {
