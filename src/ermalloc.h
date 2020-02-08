149ee1f7350e8932f29c124157522f4160776750
Dillon Hammond <dillonhammond@gmail.com>
2020-02-08 12:44:55 -0600

    read buf

--- src/ermalloc.h
+++ src/ermalloc.h
@@ -61,11 +61,27 @@ void* erreallocarray(void* ptr, size_t nmemb, size_t size, struct ermalloc_polic
 void er_change_policies(void* ptr, struct ermalloc_policy_list* policies);
 
 /**
- * Use policies to find bit errors and correct them if possible
+ * Use policies to find bit errors and correct them if possible and desired
  *
  * @return = 0 if no errors
  *         < 0 if unrecoverable errors, as defined by the associated policies
  *         > 0 number of errors found/corrected, as defined by the associated policies
  */
 int er_enforce_policies(void* ptr);
+
+/**
+ * Enforce the policy and read the data
+ * Depending on the policies selected,
+ * the policy enforcement may act on the
+ * entire allocated block, rather than just the desired region
+ *
+ * @param ptr Pointer to start of allocation
+ * @param dest Pointer to destination buffer
+ * @param offset Bytes after ptr to start reading from
+ * @param len Number of bytes to read
+ * @return = 0 if no errors
+ *         < 0 if unrecoverable errors, as defined by the associated policies
+ *         > 0 number of errors found/corrected, as defined by the associated policies
+ */
+int er_read_buf(void* ptr, void* dest, size_t offset, size_t len);
 
