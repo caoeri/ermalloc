cb018a4020877bd6dbab29f33dff53af04b1950e
Dillon Hammond <dillonhammond@gmail.com>
2020-02-08 13:00:16 -0600

    implement wrapper functions in C

--- src/ermalloc.h
+++ src/ermalloc.h
@@ -25,19 +25,19 @@ void* reallocarray(void* ptr, size_t nmemb, size_t size);
  *
  * @param policies policies for the region, NULL for no policies
  */
-void* ermalloc(size_t size, struct ermalloc_policy_list* policies);
+void* er_malloc(size_t size, struct ermalloc_policy_list* policies);
 
 /**
  * Same as free
  */
-void  erfree(void* ptr);
+void  er_free(void* ptr);
 
 /**
  * Allocate memory and zero it out
  *
  * @param policies policies for the region, NULL for no policies
  */
-void* ercalloc(size_t nmemb, size_t size, struct ermalloc_policy_list* policies);
+void* er_calloc(size_t nmemb, size_t size, struct ermalloc_policy_list* policies);
 
 /**
  * Reallocate and resize a block of memory
@@ -45,7 +45,7 @@ void* ercalloc(size_t nmemb, size_t size, struct ermalloc_policy_list* policies)
  * @param policies The policies to apply to the newly allocated block
  * Any original policies will be used to maintain data integrity while moving the allocation
  */
-void* errealloc(void* ptr, size_t size, struct ermalloc_policy_list* policies);
+void* er_realloc(void* ptr, size_t size, struct ermalloc_policy_list* policies);
 
 /**
  * Reallocate and resize a block of memory
@@ -53,7 +53,7 @@ void* errealloc(void* ptr, size_t size, struct ermalloc_policy_list* policies);
  * @param policies The policies to apply to the newly allocated block
  * Any original policies will be used to maintain data integrity while moving the allocation
  */
-void* erreallocarray(void* ptr, size_t nmemb, size_t size, struct ermalloc_policy_list* policies);
+void* er_reallocarray(void* ptr, size_t nmemb, size_t size, struct ermalloc_policy_list* policies);
 
 /**
  * Change policies for an allocated region
