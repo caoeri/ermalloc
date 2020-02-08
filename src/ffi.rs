9fe4569d70ffe3e59faca0785a1d129288e45929
Dillon Hammond <dillonhammond@gmail.com>
2020-02-08 13:17:31 -0600

    compile code

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,16 +1,22 @@
 use libc::*;
 
+use std::ptr;
+
 #[repr(C)]
 #[derive(Copy, Clone)]
-enum ErPolicyRaw {
+pub enum ErPolicyRaw {
     Nil,
     Redundancy,
 }
 
 #[repr(C)]
-struct ErPolicyListRaw {
+pub struct ErPolicyListRaw {
     policy: ErPolicyRaw,
     policy_data: *const c_void,
     er_list_policy_raw: *const ErPolicyListRaw,
 }
 
+#[no_mangle]
+pub extern "C" fn er_malloc(size: size_t, policies: *const ErPolicyListRaw) -> *mut c_void {
+    ptr::null::<c_void>() as *mut c_void
+}
