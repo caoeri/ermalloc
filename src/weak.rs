4fcb44e8b20f22301ac8b6f35d466fa911f536a5
Dillon Hammond <dillonhammond@gmail.com>
2020-02-25 15:46:59 -0600

    from ref

--- src/weak.rs
+++ src/weak.rs
@@ -6,6 +6,12 @@ struct Weak<'a, T> {
     weak: Option<&'a T>,
 }
 
+impl<'a, T> From<&'a T> for Weak<'a, T> {
+    fn from(r: &'a T) -> Self {
+        Weak { weak: Some(r) }
+    }
+}
+
 impl<'a, T> Weak<'a, T> {
     
 }
