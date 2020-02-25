040fb7cfe46082e316506e94e7a847729431daf6
Dillon Hammond <dillonhammond@gmail.com>
2020-02-25 16:06:15 -0600

    WeakMut

--- src/weak.rs
+++ src/weak.rs
@@ -20,7 +20,7 @@ impl<'a, T> Default for Weak<'a, T> {
 
 impl<'a, T> Weak<'a, T> {
     pub unsafe fn from_ptr(ptr: *const T) -> Self {
-        Weak { weak: unsafe { Some(& *ptr) } }
+        Weak { weak: Some(& *ptr) }
     }
 
     pub fn invalidate(&mut self) {
@@ -31,3 +31,34 @@ impl<'a, T> Weak<'a, T> {
         self.weak
     }
 }
+
+struct WeakMut<'a, T> {
+    weak: Option<&'a mut T>,
+}
+
+impl<'a, T> From<&'a mut T> for WeakMut<'a, T> {
+    fn from(r: &'a mut T) -> Self {
+        WeakMut { weak: Some(r) }
+    }
+}
+
+impl<'a, T> Default for WeakMut<'a, T> {
+    fn default() -> Self {
+        WeakMut { weak: None }
+    }
+}
+
+impl<'a, T> WeakMut<'a, T> {
+    pub unsafe fn from_ptr(ptr: *mut T) -> Self {
+        WeakMut { weak: Some(&mut *ptr) }
+    }
+
+    pub fn invalidate(&mut self) {
+        self.weak = None;
+    }
+
+    pub fn get_ref_mut(&mut self) -> Option<&mut T> {
+        self.weak.take()
+    }
+}
+
